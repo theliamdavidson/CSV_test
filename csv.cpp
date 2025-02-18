6c21970c716dc23949018b640a5dcaa7f794fec4
Liam Davidson <46986875+theliamdavidson@users.noreply.github.com>
2025-02-18 07:48:57 -0800

    cleaning up the main file

--- csv.cpp
+++ csv.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+// short tech demo for my boss for a project that ended up going in a different direction. this program iterates through csv files and prints it out in console
+
 int main(){
     int cont = 1;
     while(cont){
@@ -22,22 +24,21 @@ int main(){
 
         fstream file (filename, ios::in);
         if(file.is_open()){
+                                            // add contents of the csv to a 2d array
             while(getline(file, line)){
-                //ensure row does not have pre-existing data
+                                            // ensure row object does not have pre-existing data
                 row.clear();
-                // split input into words
+                                            // split input into words
                 stringstream str(line);
                 while(getline(str, word, ','))
                 row.push_back(word);
                 content.push_back(row);
             }       
-            
         }
         else{
             cout<<"Could not open the file\n";
         }
-        
-    
+                                            // print the array out in console
         for(int i=0;i<content.size();i++){
             for(int j=0;j<content[i].size();j++){
                 cout<<content[i][j]<<" ";
